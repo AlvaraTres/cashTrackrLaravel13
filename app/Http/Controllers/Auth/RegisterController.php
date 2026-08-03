8faf7093cf4493098c2ef467f6962aa4f7bcbc32
AlvaraTres <diego.leonardo.alvarado.salas@gmail.com>
2026-08-03 19:46:14 -0400

    verificacion de registro de usuario con mailtrap

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\SignUpRequest;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -16,6 +18,12 @@ class RegisterController extends Controller
     public function store(SignUpRequest $request){
         $data = $request->validated();
 
-        User::create($data);
+        $user = User::create($data);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect()->route('verification.notice');
     }
 }
